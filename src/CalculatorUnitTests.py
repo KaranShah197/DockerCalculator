8ac2e91f7d23a156ab98b43f7324aeb2e28933e3
karanshah197 <karanshah197@gmail.com>
2019-11-03 20:08:08 -0500

    Feature: Created Calculator addition method with unit tests

--- src/CalculatorUnitTests.py
+++ src/CalculatorUnitTests.py
@@ -1,10 +1,20 @@
 import unittest
 from DockerCalculator import DockerCalculator
 
+
 class MyTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.calculator = DockerCalculator()
+
     def test_instantiate_calculator(self):
-        calculator = DockerCalculator()
-        self.assertIsInstance(calculator, DockerCalculator)
+        # calculator = DockerCalculator()
+        self.assertIsInstance(self.calculator, DockerCalculator)
+
+    def test_property_add(self):
+        self.assertEqual(self.calculator.add(2, 2), 4)
+        self.assertEqual(self.calculator.add(15, 5), 20)
+
 
 
 if __name__ == '__main__':
